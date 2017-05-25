8d1502ead8e2768f872e7b94cd674e5df95a939f
luisjg <luis.guzman@metalab.csun.edu>
2017-05-25 14:56:10 -0700

    Add a send JSON response.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,35 +6,51 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-	protected function sendResponse($data, $type, array $metadata = NULL)
-	{
-		$response = [
-			'status'   => app('Illuminate\Http\Response')->status(),
-			'success'  => app('Illuminate\Http\Response')->status() < 400 ? true : false,
-			'version'  => 'affinity-1.0',
-			'type'     => $type,
-		];
+    /**
+     * Sends the JSON response using the specified data array as well as the
+     * desired HTTP response code.
+     *
+     * @param array $data An array of data to return
+     * @param integer $code The HTTP response code to return
+     * @param boolean $success Whether the request was successful
+     *
+     * @return array JSON Response
+     */
+    public function sendJsonResponse($data, $code=200, $success=true) {
 
-		if(!is_null($metadata))
-		{
-			foreach($metadata as $key => $value)
-			{
-				$response[$key] = $value;
-			}
-		}
+        // return the response
+        return response($data, $code);
+    }
 
-		$response[$type] = $data;
+    protected function sendResponse($data, $type, array $metadata = NULL)
+    {
+        $response = [
+            'status'   => app('Illuminate\Http\Response')->status(),
+            'success'  => app('Illuminate\Http\Response')->status() < 400 ? true : false,
+            'version'  => 'affinity-1.0',
+            'type'     => $type,
+        ];
 
-		if(count($data) == 0)
-		{
-			$response['messages']['data'] = 'No records found.';
-		}
+        if(!is_null($metadata))
+        {
+            foreach($metadata as $key => $value)
+            {
+                $response[$key] = $value;
+            }
+        }
 
-		if($response['success'] == false)
-		{
-			$response['messages']['server'] = 'A server error occurred.';
-		}
+        $response[$type] = $data;
 
-		return $response;
-	}
+        if(count($data) == 0)
+        {
+            $response['messages']['data'] = 'No records found.';
+        }
+
+        if($response['success'] == false)
+        {
+            $response['messages']['server'] = 'A server error occurred.';
+        }
+
+        return $response;
+    }
 }
