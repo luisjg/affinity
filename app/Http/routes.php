6a11ba4ab0421f1126d3a9a3891774d1ab32c7de
luisjg <luis.guzman@metalab.csun.edu>
2017-05-25 14:55:22 -0700

    Update the routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,20 +5,25 @@ $app->get('/', function () {
 });
 
 $app->group(['prefix' => 'api', 'namespace' => 'App\Http\Controllers'], function() use ($app) {
-	// badges requests
-	$app->get('badges', 'BadgesController@getAllBadges');
-	$app->get('badges/members', 'BadgesController@getBadgesMember');
-	$app->get('badges/{id}', 'BadgesController@getBadge');
+    // Badge requests
+    $app->get('badges', 'BadgesController@getAllBadges');
+    $app->get('badges/{email}', 'BadgesController@getPersonsBadges');
 
-	// Intersest requests
-	$app->get('interests', 'InterestsController@getInterestAll');
-	$app->get('interests/projects', 'InterestsController@getInterestwithProjects');
-	$app->get('interests/projects/{id}', 'InterestsController@getInterestProject');
+    // Interest requests
+    $app->get('interests', 'InterestsController@getAllInterests');
+    $app->get('interests/{email}', 'InterestsController@getPersonsInterests');
+//    $app->get('interests/{type}/{email}', 'InterestsController@getSpecificPersonsInterestType');
+//    $app->get('interests/{email}/{type}', 'InterestsController@getSpecificPersonsInterestType');
 
-	$app->get('interests/members', 'InterestsController@getInterestWithMembers');
-	$app->get('interests/{type}', 'InterestsController@getInterestType');
-	$app->get('interests/{type}/projects', 'InterestsController@getInterestTypeProjects');
-	$app->get('interests/{type}/members', 'InterestsController@getInterestwithMembers');
+//  Not sure if these routes will be used
+//	$app->get('badges/members', 'BadgesController@getBadgesWithMembers');
+//	$app->get('badges/{id}', 'BadgesController@getBadge');
+
+//	$app->get('interests/projects', 'InterestsController@getInterestwithProjects');
+//	$app->get('interests/projects/{id}', 'InterestsController@getInterestProject');
+//	$app->get('interests/members', 'InterestsController@getInterestWithMembers');
+//	$app->get('interests/{type}', 'InterestsController@getInterestType');
+//	$app->get('interests/{type}/projects', 'InterestsController@getInterestTypeProjects');
 
 });
 
