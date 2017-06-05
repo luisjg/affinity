362d45c88556b48620c8fc40418fdfc7f7f70f48
luisjg <luis.guzman@metalab.csun.edu>
2017-06-05 14:45:05 -0700

    Re-work the sendResponse method.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -16,41 +16,9 @@ class Controller extends BaseController
      *
      * @return array JSON Response
      */
-    public function sendJsonResponse($data, $code=200, $success=true) {
+    public function sendResponse($data, $code=200, $success=true) {
 
         // return the response
         return response($data, $code);
-    }
-
-    protected function sendResponse($data, $type, array $metadata = NULL)
-    {
-        $response = [
-            'status'   => app('Illuminate\Http\Response')->status(),
-            'success'  => app('Illuminate\Http\Response')->status() < 400 ? true : false,
-            'version'  => 'affinity-1.0',
-            'type'     => $type,
-        ];
-
-        if(!is_null($metadata))
-        {
-            foreach($metadata as $key => $value)
-            {
-                $response[$key] = $value;
-            }
-        }
-
-        $response[$type] = $data;
-
-        if(count($data) == 0)
-        {
-            $response['messages']['data'] = 'No records found.';
-        }
-
-        if($response['success'] == false)
-        {
-            $response['messages']['server'] = 'A server error occurred.';
-        }
-
-        return $response;
     }
 }
