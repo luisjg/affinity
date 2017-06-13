82cc9905f440427d78f79648e181cade3f60e0ea
luisjg <luis.guzman@metalab.csun.edu>
2017-06-13 15:53:36 -0700

    Remove certain items from the side nav panel.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,8 +72,6 @@
           <li class="nav__item"><a class="nav__link" href="#getting-started">Getting Started</a></li>
           <li class="nav__item"><a class="nav__link" href="#collections">Collections</a></li>
           <li class="nav__item"><a class="nav__link" href="#subcollections">Subcollections</a></li>
-          <li class="nav__item"><a class="nav__link" href="#instances">Instances</a></li>
-          <li class="nav__item"><a class="nav__link" href="#query">Query</a></li>
         </ul>
       </div>
 
