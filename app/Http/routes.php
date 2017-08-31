a3c480be700ff90095584f1083ca45d853f97016
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2017-08-31 10:26:22 -0700

    Deleted the commented-out routes (the unused ones)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,19 +12,6 @@ $app->group(['prefix' => '/1.0', 'namespace' => 'App\Http\Controllers'], functio
     // Interest requests
     $app->get('interests', 'InterestsController@getAllInterests');
     $app->get('interests/{type}', 'InterestsController@handleInterestType');
-//    $app->get('interests/{email}', 'InterestsController@getPersonsInterests');
-//    $app->get('interests/{type}/{email}', 'InterestsController@getSpecificPersonsInterestType');
-//    $app->get('interests/{email}/{type}', 'InterestsController@getSpecificPersonsInterestType');
-
-//  Not sure if these routes will be used
-//	$app->get('badges/members', 'BadgesController@getBadgesWithMembers');
-//	$app->get('badges/{id}', 'BadgesController@getBadge');
-
-//	$app->get('interests/projects', 'InterestsController@getInterestwithProjects');
-//	$app->get('interests/projects/{id}', 'InterestsController@getInterestProject');
-//	$app->get('interests/members', 'InterestsController@getInterestWithMembers');
-//	$app->get('interests/{type}', 'InterestsController@getInterestType');
-//	$app->get('interests/{type}/projects', 'InterestsController@getInterestTypeProjects');
 
 });
 
