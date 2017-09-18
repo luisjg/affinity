9480b5abb94330b90bba92e6d15ec266dd17f50b
joesdev <joesilvadev@gmail.com>
2017-09-18 09:26:56 -0700

    Removed email parameter route in route.php and link in home.blade

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,6 @@ $app->get('/', function () {
 $app->group(['prefix' => '/1.0', 'namespace' => 'App\Http\Controllers'], function() use ($app) {
     // Badge requests
     $app->get('badges', 'BadgesController@getAllBadges');
-    $app->get('badges/{email}', 'BadgesController@getPersonsBadges');
 
     // Interest requests
     $app->get('interests', 'InterestsController@getAllInterests');
