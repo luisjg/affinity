f9e19962bcee749f8d8999f63e2aaa9e70b53372
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2017-09-19 14:01:13 -0700

    Make touchups to the layout, cleaned it up.

--- resources/views/pages/about/version-history.blade.php
+++ resources/views/pages/about/version-history.blade.php
@@ -13,46 +13,7 @@
 	<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
 	<link rel="stylesheet" href="{{ url('css/metaphor.css') }}">
 	<link rel="stylesheet" href="{{ url('css/tomorrow.css.min') }}">
-	<style>
-		/* Style the tab */
-		div.tab {
-			overflow: hidden;
-		}
 
-		/* Style the buttons inside the tab */
-		div.tab button {
-			background-color: #f1f1f1;
-			float: left;
-			border: none;
-			outline: none;
-			cursor: pointer;
-			padding: 14px 16px;
-			transition: 0.3s;
-			font-size: 17px;
-		}
-
-		/* Change background color of buttons on hover */
-		div.tab button:hover {
-			background-color: #ddd;
-		}
-
-		/* Create an active/current tablink class */
-		div.tab button.active {
-			background-color: #ccc;
-		}
-
-		/* Style the tab content */
-		.tabcontent {
-			padding: 6px 12px;
-			border: 2px solid #ccc;
-			border-top: none;
-		}
-
-		.inactive {
-			display: none;
-		}
-
-	</style>
 </head>
 <body>
 <div class="section section--sm">
@@ -62,55 +23,62 @@
 	</div>
 </div>
 
-<div class="section">
+<div class="section" style="min-height: calc(100vh - 130px);">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
 				<a class="header--sm" href="{{ url('/') }}"><strong>DOCUMENTATION</strong></a>
 				<ul class="nav">
-					<li class="nav__item"><a class="nav__link" href="{{url('/#introduction')}}">Introduction</a></li>
-					<li class="nav__item"><a class="nav__link" href="{{url('/#getting-started')}}">Getting Started</a></li>
-					<li class="nav__item"><a class="nav__link" href="{{url('/#collections')}}">Collections</a></li>
-					<li class="nav__item"><a class="nav__link" href="{{url('/#subcollections')}}">Subcollections</a></li>
+					<li class="nav__item"><a class="nav__link" href="#introduction">Introduction</a></li>
+					<li class="nav__item"><a class="nav__link" href="#getting-started">Getting Started</a></li>
+					<li class="nav__item"><a class="nav__link" href="#collections">Collections</a></li>
+					<li class="nav__item"><a class="nav__link" href="#subcollections">Subcollections</a></li>
 				</ul>
 				<a class="header--sm" href="{{ url('/about/version-history') }}"><strong>Version History</strong></a>
 
 			</div>
 
 			<div class="col-md-9">
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-12">
-							<h1 class="page-header">Version History</h1>
-						</div>
-						<div class="col-sm-2">
-
-						</div>
-						<div class="col-sm-10">
+				<h2 id="introduction" class="type--header type--thin">Version History</h2>
+				<h2>Affinity X <small>Release Date:</small></h2>
+				<p>
+					<strong>New Features:</strong>
+				<ol>
+					<li>Placeholder</li>
 
 
-							<h2>Affinity X <small>Release Date:</small></h2>
-							<p>
-								<strong>New Features:</strong>
-							<ol>
-								<li>Placeholder</li>
+				</ol>
+				</p>
+				<hr>
 
-
-							</ol>
-							</p>
-							<hr>
-
-							<h2>Affinity 1.0 <small>Release Date:</small></h2>
-							<p>Initial Release</p>
-
-						</div>
+				<h2>Affinity 1.0 <small>Release Date:</small></h2>
+				<p>Initial Release</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="metalab-footer">
+	<div class="metalab-wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="metalab-branding">
+						<img src="//www.csun.edu/faculty/imgs/meta-logo-horz.png" alt="CSUN META Lab Logo">
+						<ul class="list--unstyled">
+							<li><a href="http://metalab.csun.edu">metalab.csun.edu</a></li>
+						</ul>
 					</div>
 				</div>
+				<div class="col-sm-6">
+					<ul class="list--unstyled metalab-tagline">
+						<li>Explore. Learn. Go Beyond.</li>
+					</ul>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
 
 <!--
   __  __   ___   _____     _
@@ -124,4 +92,3 @@
 -->
 </body>
 </html>
-
