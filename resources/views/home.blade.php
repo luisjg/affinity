358596757efd77089c50baf290ac1772f7c68f2c
joesdev <joesilvadev@gmail.com>
2017-09-20 14:20:18 -0700

    Removed hardcoded emails, used dynamic email on line 164

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -161,7 +161,7 @@
         <strong>Specified persons by Badge</strong>
         <ul>
           <li><a href="{{url('/1.0/badges?name=Teaching Conference Grant')}}">{{url('api/1.0/badges?name=Teaching Conference Grant')}}</a></li>
-          <li><a href="{{url('/1.0/badges?name=Teaching Conference Grant&email=nr_alexandra.monchick@csun.edu')}}">{{url('api/1.0/badges?name=Teaching Conference Grant&email=nr_alexandra.monchick@csun.edu')}}</a></li>
+          <li><a href="{{url('/1.0/badges?name=Teaching Conference Grant&email='.$email['alexandra'])}}">{{url('api/1.0/badges?name=Teaching Conference Grant&email='.$email['alexandra'])}}</a></li>
         </ul>
         <strong>Specified person's Interests</strong>
         <ul>
