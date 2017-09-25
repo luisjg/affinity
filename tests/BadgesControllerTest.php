c5c19b85f36bb116c73071b158cdde2b1c563664
joesdev <joesilvadev@gmail.com>
2017-09-25 12:21:05 -0700

    Removed spacing

--- tests/BadgesControllerTest.php
+++ tests/BadgesControllerTest.php
@@ -3,11 +3,9 @@ use App\Http\Controllers\BadgesController;
 
 class BadgesControllerTest extends TestCase
 {
-
     public function testCheckIfBadgeNameExists_returns_true(){
         $badgesController = new BadgesController;
         $data = $badgesController->checkIfBadgeNameExists('Teaching Conference Grant');
         $this->assertEquals(true, $data);
     }
-
 }
