6531379b780c900eba7ad10bfa7fd3cd2ab34247
joesdev <joesilvadev@gmail.com>
2017-09-27 10:47:20 -0700

    Added a function to test BadgesController@getAllBAdges
    The test function testGetAllBadges_returns_status_code_and_badge_count() checks for a correct status code and a count of badges

--- tests/BadgesControllerTest.php
+++ tests/BadgesControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\BadgesController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
 
 class BadgesControllerTest extends TestCase
 {
@@ -14,5 +15,14 @@ class BadgesControllerTest extends TestCase
         $badgesController = new BadgesController;
         $this->setExpectedException(NotFoundHttpException::class);
         $badgesController->checkIfBadgeNameExists('A non-existent badge name');
+    }
+
+    public function testGetAllBadges_returns_status_code_and_badge_count(){
+        $badgesController = new BadgesController;
+        $data = $badgesController->getAllBadges(new Request());
+        $content = json_decode($data->content(), true);
+        $this->assertEquals(200,$content['status']);
+        $this->assertEquals(11,$content['count']);
+
     }
 }
