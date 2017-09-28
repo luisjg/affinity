c17f68e0ee8e39670f095c5465b3c46863192dc1
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2017-09-28 11:12:36 -0700

    Readjusted the class to merge better with future branches

--- tests/InterestsControllerTest.php
+++ tests/InterestsControllerTest.php
@@ -4,9 +4,14 @@ use App\Http\Controllers\InterestsController;
 
 class InterestsControllerTest extends TestCase
 {
+    protected $interestController;
+
+    public function setUp(){
+        $this->interestController = new InterestsController;
+        }
     public function testGetAllResearchInterest_returns_all_research_interests(){
-        $interestController = new InterestsController;
-        $data = $interestController->getAllResearchInterests();
+        $this->interestController = new InterestsController;
+        $data = $this->interestController->getAllResearchInterests();
         $this->assertEquals($data->status(),200);
         $content = json_decode($data->content(),true);
         $this->assertEquals($content['count'],1307);
