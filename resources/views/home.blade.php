4285f6c4dbef6787af79bdcb16a731a9dc75761d
luisjg <luisjguzman87@gmail.com>
2018-02-06 08:21:11 -0800

    Re-arrange the blade files.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,89 +1,61 @@
-<!DOCTYPE HTML>
-<html class="no-js" lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.master')
 
-  <title>Affinity Web Service</title>
-  <meta name="description" content="A Web Service that delivers information on faculty engagement">
-  <link rel="icon" href="//www.csun.edu/sites/default/themes/csun/favicon.ico" type="image/x-icon">
-  <script src="//use.typekit.net/gfb2mjm.js"></script>
-  <script>try{Typekit.load();}catch(e){}</script>
-  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
-  <link rel="stylesheet" href="{!! url('css/metaphor.css') !!}">
-  <link rel="stylesheet" href="{!! url('css/tomorrow.css.min') !!}">
-  <style>
-    /* Style the tab */
-    div.tab {
-      overflow: hidden;
-    }
+@section('page-styles')
+<style>
+  /* Style the tab */
+  div.tab {
+    overflow: hidden;
+  }
 
-    /* Style the buttons inside the tab */
-    div.tab button {
-      background-color: #f1f1f1;
-      float: left;
-      border: none;
-      outline: none;
-      cursor: pointer;
-      padding: 14px 16px;
-      transition: 0.3s;
-      font-size: 17px;
-    }
+  /* Style the buttons inside the tab */
+  div.tab button {
+    background-color: #f1f1f1;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+    font-size: 17px;
+  }
 
-    /* Change background color of buttons on hover */
-    div.tab button:hover {
-      background-color: #ddd;
-    }
+  /* Change background color of buttons on hover */
+  div.tab button:hover {
+    background-color: #ddd;
+  }
 
-    /* Create an active/current tablink class */
-    div.tab button.active {
-      background-color: #ccc;
-      font-weight: bold;
-    }
+  /* Create an active/current tablink class */
+  div.tab button.active {
+    background-color: #ccc;
+    font-weight: bold;
+  }
 
-    /* Style the tab content */
-    .tabcontent {
-      padding: 6px 12px;
-      border: 2px solid #ccc;
-      border-top: none;
-    }
+  /* Style the tab content */
+  .tabcontent {
+    padding: 6px 12px;
+    border: 2px solid #ccc;
+    border-top: none;
+  }
 
-    .inactive {
-      display: none;
-    }
+  .inactive {
+    display: none;
+  }
 
-  </style>
-</head>
-<body>
-<div class="section section--sm">
-  <div class="container type--center">
-    <h1 class="giga type--thin">Affinity Web Service</h1>
-    <h3 class="h1 type--thin type--gray">Delivering Faculty Interests &amp; Badges</h3>
-  </div>
-</div>
+</style>
+@endsection
 
-<div class="section">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <p class="header--sm"><strong>DOCUMENTATION</strong></p>
-        <ul class="nav">
-          <li class="nav__item"><a class="nav__link" href="#introduction">Introduction</a></li>
-          <li class="nav__item"><a class="nav__link" href="#getting-started">Getting Started</a></li>
-          <li class="nav__item"><a class="nav__link" href="#collections">Collections</a></li>
-          <li class="nav__item"><a class="nav__link" href="#subcollections">Subcollections</a></li>
-          <li class="nav__item"><a class="nav__link" href="#code-examples">Code Examples</a></li>
-        </ul>
-        <p class="header--sm"><strong>VERSION HISTORY</strong></p>
-        <ul class="nav">
-            <li class="nav__item"><a class="nav__link" href="{{ url('/about/version-history') }}">Recent Changes</a></li>
-        </ul>
-      </div>
-
-      <div class="col-md-9">
+@section('content')
         <h2 id="introduction" class="type--header type--thin">Introduction</h2>
-        <p>The Affinity web service gives information acknowledging and celebrating teaching interests and accomplishments and helps promote faculty community and networking. This information is derived from the Research and Graduate Studies and faculty submited information using <a href="">Scholarships</a>. The web service provides a gateway to access the information via a REST-ful API. The information is retrieved by creating a specific URI and giving values to filter the data. The information that is returned is a JSON object that contains a set of interest or badges attached to a particular member; the format of the JSON object is as follows:</p>
+        <p>The Affinity web service gives information acknowledging and celebrating
+          teaching interests and accomplishments and helps promote faculty community
+          and networking. This information is derived from the Research and Graduate
+          Studies and faculty submited information using <a href="">Scholarship</a>.
+          The web service provides a gateway to access the information via a REST-ful
+          API. The information is retrieved by creating a specific URI and giving
+          values to filter the data. The information that is returned is a JSON
+          object that contains a set of interest or badges attached to a particular
+          member; the format of the JSON object is as follows:
+        </p>
 
         <div class="tab">
           <button id="badges-btn" class="tablinks active">Badges</button>
@@ -275,78 +247,9 @@ badges['badges'].each do |badge|
 						</pre>
           </dd>
         </dl>
-      </div>
-    </div>
-  </div>
-</div>
+@endsection
 
-<footer>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-5">
-        <div class="row">
-          <div class="col-sm-3 footer-seal">
-            <img src="//www.csun.edu/faculty/imgs/footer-seal.png" alt="Seal for California State University, Northridge">
-          </div>
-          <div class="col-sm-9">
-            <ul class="list--unstyled">
-              <li><strong>Affinity Web Service</strong> <br>© California State University, Northridge</li>
-              <li>18111 Nordhoff Street, Northridge, CA 91330</li>
-              <li>Phone: (818) 677-1200 / <a href="//www.csun.edu/contact/" target="_blank">Contact Us</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-7">
-        <div class="row">
-          <div class="col-sm-4">
-            <ul class="list--unstyled">
-              <li><a href="//www.csun.edu/emergency/" target="_blank">Emergency Information</a></li>
-              <li><a href="//www.csun.edu/afvp/university-policies-procedures/" target="_blank">University Policies &amp; Procedures</a></li>
-            </ul>
-          </div>
-          <div class="col-sm-4">
-            <ul class="list--unstyled">
-              <li><a href="//www.csun.edu/sites/default/files/900-12.pdf" target="_blank">Terms and Conditions for Use</a></li>
-              <li><a href="//www.csun.edu/sites/default/files/500-8025.pdf" target="_blank">Privacy Policy</a></li>
-              <li><a href="//www.csun.edu/it/document-viewers" target="_blank">Document Reader</a></li>
-            </ul>
-          </div>
-          <div class="col-sm-4">
-            <ul class="list--unstyled">
-              <li><a href="//www.calstate.edu/" target="_blank">California State University</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
-
-<div class="metalab-footer">
-  <div class="metalab-wrapper">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="metalab-branding">
-            <img src="//www.csun.edu/faculty/imgs/meta-logo-horz.png" alt="CSUN META Lab Logo">
-            <ul class="list--unstyled">
-              <li><a href="//metalab.csun.edu">metalab.csun.edu</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <ul class="list--unstyled metalab-tagline">
-            <li>Explore. Learn. Go Beyond.</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script src="{!! url('js/metaphor.js') !!}"></script>
-<script src="{!! url('js/run_prettify.js') !!}"></script>
+@section('scripts')
 <script>
   $(document).ready(function(){
       $("#badges-btn").click(function(){
@@ -364,15 +267,4 @@ badges['badges'].each do |badge|
       });
   });
 </script>
-<!--
-  __  __   ___   _____     _
- |  \/  | | __| |_   _|   /_\       Explore Learn Go Beyond
- | |\/| | | _|    | |    / _ \      https://www.metalab.csun.edu/
- |_|  |_| |___|   |_|   /_/ \_\
-    _       _        _     ___
-  _| |_    | |      /_\   | _ )
- |_   _|   | |__   / _ \  | _ \
-   |_|     |____| /_/ \_\ |___/
--->
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/partials/side-nav.blade.php
@@ -0,0 +1,12 @@
+<p class="header--sm"><strong>DOCUMENTATION</strong></p>
+<ul class="nav">
+  <li class="nav__item"><a class="nav__link" href="#introduction">Introduction</a></li>
+  <li class="nav__item"><a class="nav__link" href="#getting-started">Getting Started</a></li>
+  <li class="nav__item"><a class="nav__link" href="#collections">Collections</a></li>
+  <li class="nav__item"><a class="nav__link" href="#subcollections">Subcollections</a></li>
+  <li class="nav__item"><a class="nav__link" href="#code-examples">Code Examples</a></li>
+</ul>
+<p class="header--sm"><strong>VERSION HISTORY</strong></p>
+<ul class="nav">
+  <li class="nav__item"><a class="nav__link" href="{{ url('/about/version-history') }}">Recent Changes</a></li>
+</ul>
